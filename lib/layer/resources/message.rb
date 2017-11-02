847dc86926db91939c1341b5606e7b8577849536
Armando Mendoza <fcoarmandomendoza@gmail.com>
2017-11-02 16:40:43 -0400

    Add update_parts method to Message resource

--- lib/layer/resources/message.rb
+++ lib/layer/resources/message.rb
@@ -5,7 +5,20 @@ module Layer
         client.delete(delete_url)
       end
 
+      def update_parts(params)
+        client.patch(
+          update_parts_url,
+          body: params.to_json,
+          headers: client.layer_patch_header
+        )
+      end
+
       private
+
+      def update_parts_url
+        conversation_url = conversation["id"].split("layer:///").last
+        "#{conversation_url}/messages/#{uuid}/parts"
+      end
 
       def delete_url
         conversation_url = conversation["id"].split("layer:///").last
